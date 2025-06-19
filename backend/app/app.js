9f95d3012350a9402047118686d8c35aa3f081a2
jitendra-mark <jitendrasaini@markanthony.co.in>
2025-06-19 17:32:33 +0530

    feat: Initialize frontend project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development and build.
    - Included Vite logo SVG in the public directory.
    - Created main application structure with App component, Navbar, and Footer.
    - Implemented AddTask component for adding and updating tasks.
    - Developed Dashboard component to display tasks with delete functionality.
    - Created Footer component for navigation and branding.
    - Implemented Login and Register components for user authentication.
    - Developed Navbar for navigation and user session management.
    - Created TaskCard component for individual task representation.
    - Set up Context API for global state management.
    - Implemented Home page with conditional navigation based on authentication.
    - Defined routing structure with AllRoutes and ProtectRoutes for protected routes.
    - Added global CSS with Tailwind CSS integration.
    - Configured Vite for React and Tailwind CSS support.

---
+++ backend/app/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+import userRoutes from "../routes/user.route.js";
+import taskRoutes from "../routes/task.route.js";
+
+
+const app = express();
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(cookieParser());
+app.get("/health-check", (req, res) => res.status(200).send("Server is live..."));
+app.use(userRoutes);
+app.use(taskRoutes);
+
+export default app;
